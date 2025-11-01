1ee9fe0d6295eeb310d3e7f6506f1f8d05420213
Wesley-Maia <maia.weol@gmail.com>
2025-11-01 16:50:06 -0300

    Feature: create hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Footer from '@/src/components/layout/Footer';
 import Header from '@/src/components/layout/Header';
+import Hero from '@/src/components/sections/Hero';
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Header /> 
+      <Hero />
       <Footer />
     </main>
   );
